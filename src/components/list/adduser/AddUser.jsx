8b804b77faeeac26f2ebf62b872270c3d00a9b62
HAMMER17 <plutalov70@gmail.com>
2024-06-25 18:02:56 +0300

    addUser

--- src/components/list/adduser/AddUser.jsx
+++ src/components/list/adduser/AddUser.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { collection, getDocs, query, where } from "firebase/firestore";
 import './adduser.css'
+import { db } from '../../../lib/firebase';
 
 const AddUser = () => {
+  const [user, setUser] = useState(null)
+
+  const handleSearh = async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const name = formData.get('name')
+    try {
+      const userRef = collection(db, "users");
+      const q = query(userRef, where("name", "==", name));
+
+      const querySnapShot = await getDocs(q)
+      if (!querySnapShot.empty) {
+        setUser(querySnapShot.docs[0].data())
+      }
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
   return (
     <div className='adduser'>
-      <form >
+      <form onSubmit={handleSearh}>
         <input type="text" placeholder='Name...' name='name' />
         <button>Search</button>
       </form>
-      <div className="user">
+      {user && <div className="user">
         <div className='user_img'>
-          <img src="smile.jpg" alt="smile" />
-          <h3>User</h3>
+          <img src={user.ava || "smile.jpg"} alt="smile" />
+          <h3>{user.name}</h3>
         </div>
-
         <button>Add User</button>
-      </div>
+      </div>}
     </div>
   )
 }
--- src/components/list/chatlist/ChatList.jsx
+++ src/components/list/chatlist/ChatList.jsx
@@ -1,10 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
+
+import { useUserStore } from '../../../lib/store'
 import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
 import './chatlist.css'
 import AddUser from '../adduser/AddUser';
+import { db } from '../../../lib/firebase';
 
 const ChatList = () => {
+  const { currentUser } = useUserStore()
   const [add, setAdd] = useState(false)
+  const [chat, setChat] = useState([])
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "userschat", currentUser.id), async (doc) => {
+      const item = await doc.data().chats
+
+      const promises = item.map(async (item) => {
+        const docUserRef = doc(db, "users", item.resiverId);
+        const docUserSnap = await getDoc(docUserRef);
+
+        const user = docUserSnap.data()
+        return { ...item, user }
+      })
+      const chatData = await Promise.all(promises)
+      setChat(chatData.sort((a, b) => b.updateAt - a.updateAt))
+    });
+    return () => unSub()
+  }, [currentUser.id])
+  console.log(chat)
   return (
     <div className='chatlist'>
       <div className='chatsearch'>
@@ -15,49 +39,15 @@ const ChatList = () => {
         {add ? <FaMinusCircle className='chaticon' onClick={() => setAdd(pre => !pre)} /> :
           <FaPlusCircle className='chaticon' onClick={() => setAdd(pre => !pre)} />}
       </div>
-
-      <div className='chatitem'>
-        <img src="/ava.jpg" alt="avatar" />
-        <div>
-          <h3>Hammer</h3>
-          <p>Weclcome to my chat</p>
+      {chat.map((data) => (
+        <div className='chatitem' key={data.chatId}>
+          <img src="/ava.jpg" alt="avatar" />
+          <div>
+            <h3>Hammer</h3>
+            <p>{data.lastMessage}</p>
+          </div>
         </div>
-      </div>
-      <div className='chatitem'>
-        <img src="/ava.jpg" alt="avatar" />
-        <div>
-          <h3>User</h3>
-          <p>Weclcome to my chat</p>
-        </div>
-      </div>
-      <div className='chatitem'>
-        <img src="/ava.jpg" alt="avatar" />
-        <div>
-          <h3>User</h3>
-          <p>Weclcome to my chat</p>
-        </div>
-      </div>
-      <div className='chatitem'>
-        <img src="/ava.jpg" alt="avatar" />
-        <div>
-          <h3>User</h3>
-          <p>Weclcome to my chat</p>
-        </div>
-      </div>
-      <div className='chatitem'>
-        <img src="/ava.jpg" alt="avatar" />
-        <div>
-          <h3>User</h3>
-          <p>Weclcome to my chat</p>
-        </div>
-      </div>
-      <div className='chatitem'>
-        <img src="/ava.jpg" alt="avatar" />
-        <div>
-          <h3>User</h3>
-          <p>Weclcome to my chat</p>
-        </div>
-      </div>
+      ))}
 
 
       {add && <AddUser />}
